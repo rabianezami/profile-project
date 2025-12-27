bb00dc57ff097d4a0fb70e4e07aa01c5106c75a9
rabia zia nezami <rabianezami786@gail.com>
2025-12-27 10:39:17 +0430

    add data and import profilePage

--- profile-project/src/App.jsx
+++ profile-project/src/App.jsx
@@ -1,12 +1,37 @@
+import ProfilePage from "./components/ProfilePage";
+import avatarImage from "./assets/avatar.jpg";
 
+export default function App() {
+  const user = {
+    name: "Rabia Zia Nezai",
+    username: "@rabianezami",
+    avatarImage,
+    xp: 80,
+    lastActive: "Dec 24, 2025",
+    streakDays: 2,
+    longestStreak: 3,
+    recentBadgesText: "No badges earned yet.",
+    personalInfo: {
+      Email: "rabianezami786@gmail.com",
+      Gender: "Famle",
+      Country: "Afghanistan",
+      "Province or State": "Parwan",
+      "Date Of Birth": "2/8/2001",
+      "Online Portfolio Link": "N/A",
+      "LinkedIn Link": "https://www.linkedin.com/in/rabia-zia-nezami-993989379/",
+      Languages: "farsi, english",
+      Bio: "Student @CodeToInspire",
+    },
+    accountDetails: {
+      Role: "Student",
+      Status: "Active",
+      Verification: "Verified",
+    },
+  };
 
-function App() {
-  
   return (
-    <>
-   
-    </>
-  )
+    <div className="min-h-screen bg-[#0b1220] text-gray-200 p-6 font-sans">
+      <ProfilePage user={user} />
+    </div>
+  );
 }
-
-export default App
